0578d172a09115f1c96ee1a3c5d5f7080bc135c5
zhineng <zhineng@gmail.com>
2022-06-05 18:09:34 +0800

    5.8.3個常見的動態網頁等待時機

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import sys
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
@@ -18,9 +20,17 @@ for page in range(1, 3):
 
     # driver.get(
     #    'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
-    time.sleep(5)
+    # time.sleep(5)
 
-    #ActionChains(driver).move_by_offset(100, 100).click().perform()
+    ActionChains(driver).move_by_offset(100, 100).click().perform()
+
+    locator = (By.CSS_SELECTOR,
+               "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(locator),
+        "找不到指定的元素"
+    )
 
     cards = driver.find_elements(
         By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
@@ -28,7 +38,7 @@ for page in range(1, 3):
     items = []
 
     for card in cards:
-        ActionChains(driver).move_to_element(card).perform()
+        # ActionChains(driver).move_to_element(card).perform()
         title = card.find_element(
             By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
         price = card.find_element(
