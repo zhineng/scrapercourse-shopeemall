37905b2fd7c7f4abbe459f8587d21989c41a175d
zhineng <zhineng@gmail.com>
2022-06-05 12:17:26 +0800

    5.4 動態網頁彈跳視窗的關閉方法

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import time
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 import sys
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
@@ -10,6 +11,7 @@ driver.get(
     'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
 time.sleep(5)
 
+ActionChains(driver).move_by_offset(100, 100).click().perform()
 
 cards = driver.find_elements(
     By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
